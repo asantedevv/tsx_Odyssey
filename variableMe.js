1cd019942fc142265ea251adbf0d59cc8a6c2f0e
Kwame Asante Junior <vie97pie@gmail.com>
2024-09-05 12:02:31 +0000

    typescript syntax

--- variableMe.js
+++ variableMe.js
@@ -2,3 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Kwame";
 console.log(greetings);
+// number 
+var price = 2245.33;
+// boolean
+var isLoggedIn = false;
--- variableMe.ts
+++ variableMe.ts
@@ -2,4 +2,11 @@ let greetings: string = "Hello Kwame";
 
 console.log(greetings);
 
+// number 
+let price: number = 2245.33
+ 
+// boolean
+let isLoggedIn: boolean = false
+
+
 export {} // getting rid of errors temporarily
